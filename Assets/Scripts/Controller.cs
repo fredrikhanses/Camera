e39c7028803a0ddb3933d0028c657fa2ceac7436
Fredrik Hanses <fredrik.hanses@gmail.com>
2020-03-13 18:14:32 +0100

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+
+    [SerializeField, Range(0.0f, 100.0f)]
+    float maxSpeed = 10.0f, maxAcceleration = 10.0f, maxAirAcceleration = 1.0f;
+
+    [SerializeField, Range(0.0f, 10.0f)]
+    float jumpHeight = 2.0f;
+
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
+
+    [SerializeField, Range(0.0f, 90.0f)]
+    float maxGroundAngle = 40.0f;
+
+    Rigidbody body;
+
+    Vector3 velocity, desiredVelocity, contactNormal;
+
+    bool desiredJump;
+
+    int jumpPhase, groundContactCount;
+
+    bool OnGround => groundContactCount > 0;
+
+    float minGroundDotProduct;
+
+    void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);    
+    }
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        OnValidate();
+    }
+
+    void Update()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        playerInput = Vector2.ClampMagnitude(playerInput, 1.0f);
+        desiredVelocity = new Vector3(playerInput.x, 0.0f, playerInput.y) * maxSpeed;
+        desiredJump |= Input.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        UpdateState();
+        AdjustVelocity();
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+        body.velocity = velocity;
+        ClearState();
+    }
+
+    void UpdateState()
+    {
+        velocity = body.velocity;
+        if(OnGround)
+        {
+            jumpPhase = 0;
+            if(groundContactCount > 1)
+            {
+                contactNormal.Normalize();
+            }        
+        }
+        else
+        {
+            contactNormal = Vector3.up;
+        }
+    }
+
+    void ClearState()
+    {
+        groundContactCount = 0;
+        contactNormal = Vector3.zero;
+    }
+
+    void Jump()
+    {
+        if (OnGround || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
+            float jumpSpeed = Mathf.Sqrt(-2 * Physics.gravity.y * jumpHeight);
+            float alignedSpeed = Vector3.Dot(velocity, contactNormal);
+            if(alignedSpeed > 0.0f)
+            {
+                jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0.0f);
+            }
+            velocity += contactNormal * jumpSpeed;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void EvaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            if(normal.y >= minGroundDotProduct)
+            {
+                groundContactCount += 1;
+                contactNormal += normal;
+            }
+        }
+    }
+
+    void AdjustVelocity()
+    {
+        Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+        Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+        float currentX = Vector3.Dot(velocity, xAxis);
+        float currentZ = Vector3.Dot(velocity, zAxis);
+
+        float acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
+        float maxSpeedChange = acceleration * Time.fixedDeltaTime;
+
+        float newX = Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
+        float newZ = Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
+
+        velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+    }
+
+    Vector3 ProjectOnContactPlane(Vector3 vector)
+    {
+        return vector - contactNormal * Vector3.Dot(vector, contactNormal);
+    }
+}
---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform focus = default;
+
+    [SerializeField, Range(1.0f, 20.0f)]
+    float distance = 5.0f;
+
+    [SerializeField, Min(0.0f)]
+    float focusRadius = 1.0f;
+
+    [SerializeField, Range(0.0f, 1.0f)]
+    float focusCentering = 0.75f;
+
+    [SerializeField, Range(1.0f, 360.0f)]
+    float rotationSpeed = 90.0f;
+
+    [SerializeField, Range(-89.0f, 89.0f)]
+    float minVerticalAngle = -30.0f, maxVerticalAngle = 60.0f;
+
+    [SerializeField, Min(0.0f)]
+    float alignDelay = 5.0f;
+
+    Vector3 focusPoint, previousFocusPoint;
+
+    Vector2 orbitAngles = new Vector2(45.0f, 0.0f);
+
+    float lastManualRotationTime;
+
+    void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+
+    void Awake()
+    {
+        focusPoint = focus.position;
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+    }
+
+    void LateUpdate()
+    {
+        UpdateFocusPoint();
+        ManualRotation();
+        Quaternion lookRotation;
+        if (ManualRotation() || AutomaticRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else
+        {
+            lookRotation = transform.localRotation;
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+
+    void UpdateFocusPoint()
+    {        
+        Vector3 targetPoint = focus.position;
+        if(focusRadius > 0.0f)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            if(distance > focusRadius)
+            {
+                focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+            }
+        }
+        if (distance > 0.01f && focusCentering > 0.0f)
+        {
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, Mathf.Pow(1.0f - focusCentering, Time.unscaledDeltaTime));
+        }
+    }
+
+    bool ManualRotation()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Vertical Camera"), Input.GetAxis("Horizontal Camera"));
+        const float e = 0.001f;
+        if(input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            lastManualRotationTime = Time.unscaledTime;
+            return true;
+        }
+        return false;
+    }
+
+    void ConstrainAngles()
+    {
+        orbitAngles.x = Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+
+        if (orbitAngles.y < 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+        else if (orbitAngles.y >= 360f)
+        {
+            orbitAngles.y -= 360f;
+        }
+    }
+
+    bool AutomaticRotation()
+    {
+        if(Time.unscaledTime - lastManualRotationTime < alignDelay)
+        {
+            return false;
+        }
+        return true;
+    }
+}
